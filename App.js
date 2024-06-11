1226eb4e048c49066a9d9aa9b3fe925c4331d46d
tomaszbialkowski <tomaszbialkowski78@gmail.com>
2024-06-11 19:10:38 +0200

    initial commit

--- App.js
+++ App.js
@@ -1,20 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { SplashScreen } from "./src/screens/SplashScreen";
+SplashScreen;
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView>
+      <Text style={styles.text}>SzlakByTo</Text>
+    </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/screens/SplashScreen.js
@@ -0,0 +1,22 @@
+import { StyleSheet } from "react-native";
+import LottieView from "lottie-react-native";
+
+import splashAnimation from "../../assets/animations/splashAnimation.json";
+
+export const SplashScreen = () => {
+  return (
+    <LottieView
+      source={splashAnimation}
+      autoPlay
+      loop
+      style={styles.container}
+    />
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    width: "100%",
+    height: "100%",
+  },
+});
